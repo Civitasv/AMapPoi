c853b9e31366dce5ea37f15f211d29eef2572c89
Civitasv <sen.hu@whu.edu.cn>
2021-04-26 15:40:49 +0800

    样式修改

--- src/main/java/com/civitasv/spider/util/MessageUtil.java
+++ src/main/java/com/civitasv/spider/util/MessageUtil.java
@@ -3,6 +3,8 @@ package com.civitasv.spider.util;
 import com.civitasv.spider.MainApplication;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -14,17 +16,22 @@ import java.awt.*;
 public class MessageUtil {
     /**
      * 信息提示框
-     * @param type {@link Alert.AlertType}
-     * @param title
-     * @param header
-     * @param content
+     *
+     * @param type    {@link Alert.AlertType}
+     * @param title   标题
+     * @param header  头信息
+     * @param content 具体内容
      */
     public static void alert(Alert.AlertType type, String title, String header, String content) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
-        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(
+                MainApplication.class.getResource("myDialogs.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialog");
+        Stage stage = (Stage) dialogPane.getScene().getWindow();
         stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("icon/icon.png")));
         alert.showAndWait();
     }
